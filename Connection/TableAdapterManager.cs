ab97b6ef775ad0942af10a42ff50eebcb37c0822
Ramiro <ramirosuriano@gmail.com>
2019-02-12 23:32:25 -0300

    Create connection in Visual Studio

---
+++ Connection/TableAdapterManager.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Web.Configuration;
+
+namespace Connection
+{
+
+    public class QTACustomized : GenesisDataSetTableAdapters.QueriesTableAdapter
+    {
+
+        public QTACustomized() 
+        {
+            ChangeConnectionString();
+        }
+
+        public void ChangeConnectionString()
+        {
+            foreach (IDbCommand i in CommandCollection)
+            {
+
+                i.Connection.ConnectionString = WebConfigurationManager.AppSettings["ConnectionString"];
+            }
+        }
+
+    }
+
+    public static class TableAdapterManager
+    {
+       public static void ChangeConnection (ref System.Data.SqlClient.SqlConnection MyConnection, string MyClass)
+       {
+
+
+
+           string connection = WebConfigurationManager.AppSettings["ConnectionString"];
+           MyConnection.ConnectionString = connection;
+           
+       
+       }
+    }
+}
