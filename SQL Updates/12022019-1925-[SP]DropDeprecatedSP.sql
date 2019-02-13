7ed293d82bf7c704fc33d94c6ff78d1968981305
Ramiro <ramirosuriano@gmail.com>
2019-02-12 21:03:32 -0300

    Delete deprecated SP and create UpdateUser

---
+++ SQL Updates/12022019-1925-[SP]DropDeprecatedSP.sql
@@ -0,0 +1,8 @@
+USE [Genesis]
+GO
+
+/****** Object:  StoredProcedure [dbo].[Get_UserByUserNameAndPassword]    Script Date: 12/2/2019 19:20:34 ******/
+DROP PROCEDURE [dbo].[Get_UserByUserNameAndPassword]
+GO
+
+
---
+++ SQL Updates/12022019-1959-[SP]UpdateUser.sql
@@ -0,0 +1,34 @@
+USE [Genesis]
+GO
+
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE PROCEDURE [dbo].[Update_User]	@Id bigint,
+										@UserName varchar(50) = NULL,
+										@Email varchar(50) = NULL,
+										@Password varchar(40) = NULL
+AS
+
+DECLARE @sql nvarchar(max);
+
+SET @sql = 'UPDATE Users SET'
+
+IF @UserName IS NOT NULL
+	SET @sql = @sql + ' UserName = @Username,';
+IF @Email IS NOT NULL
+	SET @sql = @sql + ' Email = @Email,';
+IF @Password IS NOT NULL
+	SET @sql = @sql + ' Password = @Password,';
+
+SET @sql = SUBSTRING(@sql, 1, (LEN(@sql) - 1))
+SET @sql = @sql + ' WHERE Id = @Id';
+
+EXEC sp_executesql @sql, N'
+		@Id bigint, 
+		@UserName varchar(50), 
+		@Email varchar(50), 
+		@Password varchar(40)',
+		@Id, @UserName, @Email, @Password;
