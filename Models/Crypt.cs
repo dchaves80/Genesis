c7dbd7fb4552a4bfa6f764f7549349de6dc52408
LGN501 <suprapixel21@gmail.com>
2019-02-17 17:58:04 -0300

    [Models] migración Business a Models

---
+++ Models/Crypt.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+
+namespace Models
+{
+    public class Crypt
+    {
+
+        /// <summary>
+        /// Encripta un string por MD5
+        /// </summary>
+        /// <param name="input">Strijng de entrada</param>
+        /// <returns>Devuelve un string encriptado por MD5</returns>
+        public static string StringByMD5(string input)
+        {
+            MD5 md5 = MD5.Create();
+
+            byte[] hash = md5.ComputeHash(Encoding.ASCII.GetBytes(input));
+
+            StringBuilder sb = new StringBuilder();
+            for (int a = 0; a < hash.Length; a++)
+            {
+                sb.Append(hash[a].ToString("X2"));
+            }
+
+            return sb.ToString();
+        }
+    }
+}
---
+++ Models/Mod_Users.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using Connection;
+
+namespace Models
+{
+    public class Mod_Users
+    {
+
+        /// <summary>
+        /// Se fija si las credenciales son correctas o no al loguear
+        /// </summary>
+        /// <param name="user">Username del usuario</param>
+        /// <param name="password">Password del usuario</param>
+        /// <returns>Devuelve true si son correctas, false si no</returns>
+        public static bool Get_UserByUserAndPassword (
+            string user,
+            string password)
+        {
+            DataTable DT = Con_Users.Get_User(null, user, null, password, 0);
+
+            if (DT != null && DT.Rows.Count > 0) return true;
+            else return false;
+        }
+
+    }
+}
