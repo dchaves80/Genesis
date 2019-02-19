0328e1715c742bee4c9778f40e88fbbbf831b81b
LGN501 <suprapixel21@gmail.com>
2019-02-19 18:32:25 -0300

    [SQL-Usuarios] Nuevo SP y pequeña modificacion

---
+++ Modules SLN/Core/Usuarios/Usuarios/Usuarios.ascx
@@ -0,0 +1,11 @@
+﻿<%@ Control Language="C#" AutoEventWireup="true" CodeBehind="Usuarios.ascx.cs" Inherits="Usuarios.Usuarios" %>
+
+<!-- CONTENT -->
+<div>
+
+</div>
+
+<!-- HIDDENS -->
+<div>
+
+</div>
---
+++ SQL Updates/19022019-1830-[SP]Modules.sql
@@ -0,0 +1,30 @@
+USE [Genesis]
+GO
+
+/****** Object:  Table [dbo].[Modules]    Script Date: 19/02/2019 18:31:05 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+SET ANSI_PADDING ON
+GO
+
+CREATE TABLE [dbo].[Modules](
+	[Id] [bigint] IDENTITY(1,1) NOT NULL,
+	[NombreModulo] [varchar](50) NOT NULL,
+	[Ascx] [varchar](50) NOT NULL,
+	[Dll] [varchar](50) NOT NULL,
+ CONSTRAINT [PK_Modules] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+GO
+
+SET ANSI_PADDING OFF
+GO
+
+
