ee36db50b618498a676413dc6a99bc3a54d1eb02
Ramiro <ramirosuriano@gmail.com>
2019-04-06 04:43:28 -0300

    Create Mod_Modules.cs

---
+++ Models/Mod_Modules.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Connection;
+
+namespace Models
+{
+    public class Mod_Modules
+    {
+        int id;
+        string name;
+        string path;
+        string dll;
+
+        public string Name { get => name; set => name = value; }
+        public string Path { get => path; set => path = value; }
+        public string Dll { get => dll; set => dll = value; }
+        public int Id { get => id; set => id = value; }
+
+        private Mod_Modules(DataRow DR)
+        {
+            id = int.Parse ( DR["Id"].ToString() );
+            name = DR["ModuleName"].ToString();
+            path = DR["Ascx"].ToString();
+            dll = DR["Dll"].ToString();
+        }
+
+        public static List<Mod_Modules> GetModules()
+        {
+            DataTable DT = Con_Modules.Get_Module(null, null, null, null);
+            if (DT !=null && DT.Rows.Count > 0)
+            {
+                List<Mod_Modules> aux = new List<Mod_Modules>();
+                foreach (DataRow DR in DT.Rows)
+                {
+                    aux.Add(new Mod_Modules(DR));
+                }
+                return aux;
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        public static Mod_Modules GetModuleById(int Id)
+        {
+            DataTable DT = Con_Modules.Get_Module(Id, null, null, null);
+            if (DT != null && DT.Rows.Count > 0)
+            {
+                return new Mod_Modules(DT.Rows[0]);
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+}
