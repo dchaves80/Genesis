1c2b837528d278e4138249cd8d5a5b461154d5b9
Ramiro <ramirosuriano@gmail.com>
2019-04-06 02:19:23 -0300

    Remade Con_Modules

--- Connection/Con_Modules.cs
+++ Connection/Con_Modules.cs
@@ -1,55 +1,44 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Data;
+
+////	Code generated via CodeReplicator v1.0. By Ramiro Suriano, for Olympus Software.	////
+////	Date of this code: 6/4/2019							                                ////
 
 namespace Connection
 {
-    public class Con_Modules
-    {
+	public class Con_Modules
+	{
 
-        /// <summary>
-        /// Inserts a new module to the DataBaser
-        /// </summary>
-        /// <param name="ModuleName">ModuleName</param>
-        /// <param name="Ascx">Module's ascx</param>
-        /// <param name="Dll">Module's dll</param>
-        /// <returns>Returns a DataRow with the id of the inserted user,
-        /// null if there was an error at the SQL level.</returns>
-        public static DataRow Insert_Module(
-            string ModuleName,
-            string Ascx,
-            string Dll)
-        {
-            
-            
-            GenesisDataSetTableAdapters.Insert_ModuleTableAdapter TA =
-                new GenesisDataSetTableAdapters.Insert_ModuleTableAdapter();
-            GenesisDataSet.Insert_ModuleDataTable DR =
-                new GenesisDataSet.Insert_ModuleDataTable();
-            
+		public static DataTable Insert_Module(string ModuleName, string Ascx, string Dll)
+		{
+			GenesisDataSetTableAdapters.Insert_ModuleTableAdapter TA = new GenesisDataSetTableAdapters.Insert_ModuleTableAdapter();
+			GenesisDataSet.Insert_ModuleDataTable DT = new GenesisDataSet.Insert_ModuleDataTable();
+			System.Data.SqlClient.SqlConnection SQLCONN = TA.Connection;
+			TableAdapterManager.ChangeConnection(ref SQLCONN);
+			TA.Fill(DT, ModuleName, Ascx, Dll);
+			if (DT != null && DT.Rows.Count > 0) { return DT; } else { return null; }
+		}
 
-            System.Data.SqlClient.SqlConnection SQLCONN = TA.Connection;
-            TableAdapterManager.ChangeConnection(ref SQLCONN);
+		public static DataTable Get_Module(int? Id, string ModuleName, string Ascx, string Dll)
+		{
+			GenesisDataSetTableAdapters.Get_ModuleTableAdapter TA = new GenesisDataSetTableAdapters.Get_ModuleTableAdapter();
+			GenesisDataSet.Get_ModuleDataTable DT = new GenesisDataSet.Get_ModuleDataTable();
+			System.Data.SqlClient.SqlConnection SQLCONN = TA.Connection;
+			TableAdapterManager.ChangeConnection(ref SQLCONN);
+			TA.Fill(DT, Id, ModuleName, Ascx, Dll);
+			if (DT != null && DT.Rows.Count > 0) { return DT; } else { return null; }
+		}
 
-            TA.Fill(DR, ModuleName, Ascx, Dll);
+		public static bool Delete_Module(int Id, string ModuleName)
+		{
+			QTACustomized QTA = new QTACustomized();
+			int response = QTA.Delete_Module(Id, ModuleName);
+			if (response > 0) { return true; } else { return false; }
+		}
 
-            if (DR != null && DR.Rows.Count > 0)
-                return DR[0];
-            else
-                return null;                
-        }
-
-        public static DataTable Get_Modules(
-            int? Id,
-            string ModuleName,
-            string Ascx,
-            string Dll)
-        {
-            return null;
-        }
-
-    }
+	}
 }
