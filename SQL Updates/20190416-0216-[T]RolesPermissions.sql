7b8cf6cc09b499a9f16048fe0df1ee78caadf41f
Ramiro <ramirosuriano@gmail.com>
2019-04-16 02:16:27 -0300

    Create 20190416-0216-[T]RolesPermissions.sql

---
+++ SQL Updates/20190416-0216-[T]RolesPermissions.sql
@@ -0,0 +1,29 @@
+/* To prevent any potential data loss issues, you should review this script in detail before running it outside the context of the database designer.*/
+BEGIN TRANSACTION
+SET QUOTED_IDENTIFIER ON
+SET ARITHABORT ON
+SET NUMERIC_ROUNDABORT OFF
+SET CONCAT_NULL_YIELDS_NULL ON
+SET ANSI_NULLS ON
+SET ANSI_PADDING ON
+SET ANSI_WARNINGS ON
+COMMIT
+BEGIN TRANSACTION
+GO
+CREATE TABLE dbo.RolesPermissions
+	(
+	Id bigint NOT NULL IDENTITY (1, 1),
+	RoleId bigint NOT NULL,
+	ModuleId bigint NOT NULL
+	)  ON [PRIMARY]
+GO
+ALTER TABLE dbo.RolesPermissions ADD CONSTRAINT
+	PK_RolesPermissions PRIMARY KEY CLUSTERED 
+	(
+	Id
+	) WITH( STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+
+GO
+ALTER TABLE dbo.RolesPermissions SET (LOCK_ESCALATION = TABLE)
+GO
+COMMIT
