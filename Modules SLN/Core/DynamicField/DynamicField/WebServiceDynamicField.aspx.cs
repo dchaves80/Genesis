44425998f45093c8b30ad5d9e6dc9b627c1e4fed
David Chaves <dlchaves81@gmail.com>
2019-04-28 18:28:35 -0300

    [DynamicField]

---
+++ Modules SLN/Core/DynamicField/DynamicField/WebServiceDynamicField.aspx.cs
@@ -0,0 +1,36 @@
+﻿using Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace DynamicField
+{
+    public partial class WebServiceDynamicField : System.Web.UI.Page
+    {
+        string content;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+            Response.Clear();
+            Response.ClearHeaders();
+
+            if (Request["task"] == "Update") { Update(); }
+
+            Response.Write(content);
+            Response.Flush();
+            Response.End();
+        }
+
+            void Update()
+            {
+                int id = int.Parse(Request["id"]);
+                string idval = Request["Val"];
+                Mod_DynamicField df = Models.Mod_DynamicField.GetById(id);
+                df.Update(idval);
+               content = "true";
+            }
+    }
+}
